49a106f7ce1dee378d0bbd4632ba2095f79f2ddb
osama abd al rahem <osamarahem@gmail.com>
2026-04-04 18:09:14 +0300

    Admin Panel

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
+from django.utils.translation import gettext as _
 
 # Register your models here.
+admin.site.register(models.Category)
+admin.site.register(models.Project)
+admin.site.register(models.Task)
+
+admin.site.site_header = _("Projects Management")
+admin.site.site_title = _("Projects Management")
+# admin.site.index_title = _("Projects Management")
